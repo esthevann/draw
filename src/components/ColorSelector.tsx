c371932b90e83b13299f1f3a785f77725613f900
Esthevan <esthevanferretti69@gmail.com>
2022-08-13 22:15:35 -0300

    color selection code refactored, ability to pick color by hex code

--- src/components/ColorSelector.tsx
+++ src/components/ColorSelector.tsx
@@ -1,46 +1,55 @@
+import { ChangeEvent } from "react";
 import ColorIcon from "./ColorIcon";
 
 interface ColorSelectorProps {
-    color: string;
-    setColor: (color: string) => void;
+  color: string;
+  setColor: (color: string) => void;
 }
 
-export default function ColorSelector({color, setColor}: ColorSelectorProps) {
-    return (
-        <div className="btn-group-vertical" role={"group"}>
-                <div className="btn-group">
-                  <button className="btn" onClick={() => setColor("#dc2626")}>
-                    <ColorIcon colorHex="#dc2626" colorState={color} colorTailwind="bg-red-600" />
-                  </button>
-                  <button className="btn" onClick={() => setColor("#2563eb")}>
-                    <ColorIcon colorHex="#2563eb" colorState={color} colorTailwind="bg-blue-600" />
-                  </button>
-                  <button className="btn" onClick={() => setColor("#16a34a")}>
-                    <ColorIcon colorHex="#16a34a" colorState={color} colorTailwind="bg-green-600" />
-                  </button>
-                </div>
-                <div className="btn-group">
-                  <button className="btn" onClick={() => setColor("#000000")}>
-                    <ColorIcon colorHex="#000000" colorState={color} colorTailwind="bg-black" />
-                  </button>
-                  <button className="btn" onClick={() => setColor("#7c3aed")}>
-                    <ColorIcon colorHex="#7c3aed" colorState={color} colorTailwind="bg-violet-600" />
-                  </button>
-                  <button className="btn" onClick={() => setColor("#ea580c")}>
-                    <ColorIcon colorHex="#ea580c" colorState={color} colorTailwind="bg-orange-600" />
-                  </button>
-                </div>
-                <div className="btn-group">
-                  <button className="btn" onClick={() => setColor("#eab308")}>
-                    <ColorIcon colorHex="#eab308" colorState={color} colorTailwind="bg-yellow-500" />
-                  </button>
-                  <button className="btn" onClick={() => setColor("#f472b6")}>
-                    <ColorIcon colorHex="#f472b6" colorState={color} colorTailwind="bg-pink-400" />
-                  </button>
-                  <button className="btn" onClick={() => setColor("#06b6d4")}>
-                    <ColorIcon colorHex="#06b6d4" colorState={color} colorTailwind="bg-cyan-500" />
-                  </button>
-                </div>
-              </div>
+export default function ColorSelector({ color, setColor }: ColorSelectorProps) {
+  // amount of colors must be always a multiple of 3
+  const colors = {
+    "#000000": "bg-black",
+    "#dc2626": "bg-red-600",
+    "#2563eb": "bg-blue-600",
+    "#16a34a": "bg-green-600",
+    "#7c3aed": "bg-violet-600",
+    "#ea580c": "bg-orange-600",
+    "#eab308": "bg-yellow-500",
+    "#f472b6": "bg-pink-400",
+    "#06b6d4": "bg-cyan-500",
+  }
+  let items: JSX.Element[] = [];
+  Object.entries(colors).map(([colorCode, colorName], i) => {
+    items.push(
+      <button className="btn" onClick={() => setColor(colorCode)} key={i}>
+        <ColorIcon colorHex={colorCode} colorState={color} colorTailwind={colorName} />
+      </button>
     )
+  })
+
+  function handleChange(e: React.MouseEvent<HTMLInputElement>) {
+    const reg = /^#([0-9a-f]{3}){1,2}$/i;
+    reg.test(e.currentTarget.value) && setColor(e.currentTarget.value);    
+  }
+
+  return (
+    <div className="btn-group-vertical" role={"group"}>
+      <div className="btn-group">
+        {items.splice(-3)}
+      </div>
+      <div className="btn-group">
+        {items.splice(-3)}
+      </div>
+      <div className="btn-group">
+        {items.splice(-3)}
+      </div>
+      <div className="btn-group">
+        <button className="btn">
+          <input type="text" placeholder={`${color}`} onClick={handleChange} className="input input-bordered w-[144px] h-6" />
+        </button>
+      </div>
+    </div>
+
+  )
 }
